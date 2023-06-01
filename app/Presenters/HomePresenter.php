f9ca60b907582686822764fc2dd5c974e2367bec
marty <marty@debian.zelina.tech>
2023-06-01 19:30:16 +0200

    prvni applikace v nette

---
+++ app/Presenters/HomePresenter.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class HomePresenter extends Nette\Application\UI\Presenter
+{
+    public function __construct(
+        private Nette\Database\Explorer $database,
+    ) {
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database
+            ->table('posts')
+            ->order('created_at DESC')
+            ->limit(5);
+    }
+}
